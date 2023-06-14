98ac8b241dfdfd9fd6204d1c18172ec8aea44423
Elham Al Haq <elhamalhaq@gmail.com>
2023-06-14 19:44:14 +0700

    update fetch and search

--- src/components/store/homeReducer/index.jsx
+++ src/components/store/homeReducer/index.jsx
@@ -1,34 +1,42 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState ={
-    movie: [],
-    loading: false,
-}
+const APIKEY = '25129c6';
+const initialState = {
+  movie: [],
+  loading: false,
+};
 
-export const getMovie = createAsyncThunk("home/getMovie", async () => {
-    const response = await axios.get(`${process.env.REACT_APP_BASEURL}?s=man&apikey=${process.env.REACT_APP_APIKEY}`);
-    console.log(response.data);
-    return response.data;
-})
+// export const getMovie = createAsyncThunk("home/getMovie", async () => {
+//     const response = await axios.get(`${process.env.REACT_APP_BASEURL}?s=man&apikey=${process.env.REACT_APP_APIKEY}`);
+//     console.log(response.data);
+//     return response.data;
+// })
+export const getMovie = createAsyncThunk('home/getMovie', async (query) => {
+  const response = await axios.get(
+    `http://www.omdbapi.com/?apikey=${APIKEY}&s=${query}`
+  );
+  console.log(response.data);
+  return response.data;
+});
 
 const homeSlice = createSlice({
-    name: "home",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-            .addCase(getMovie.pending, (state) => {
-                state.loading = true;
-            })
-            .addCase(getMovie.fulfilled, (state, { payload }) => {
-                state.movie = payload;
-                state.loading = false;
-            })
-            .addCase(getMovie.rejected, (state) => {
-                state.loading = false;
-            })
-    },
-})
+  name: 'home',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getMovie.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getMovie.fulfilled, (state, { payload }) => {
+        state.movie = payload;
+        state.loading = false;
+      })
+      .addCase(getMovie.rejected, (state) => {
+        state.loading = false;
+      });
+  },
+});
 
 export default homeSlice.reducer;
