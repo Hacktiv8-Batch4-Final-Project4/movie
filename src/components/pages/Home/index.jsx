6ed98b5d0ec3bee6e877d32829f3e7363f69908e
dirhamtriyadi <dirhamtriyadi@gmail.com>
2023-06-15 08:06:09 +0700

    feat: add Year and Type

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -29,7 +29,8 @@ const Home = () => {
               />
               <div className="card-body">
                 <h5 className="card-title">{item.Title}</h5>
-                {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+                <p className="card-text">{item.Year}</p>
+                <p className="card-text">{item.Type}</p>
               </div>
               <div className="card-footer">
                 <Link to={`/detail/${item.imdbID}`} className="btn btn-primary">
