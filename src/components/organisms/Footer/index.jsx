cccb682618fcd73b4430ea7cb3bdae28dd464319
DINDA <dindadfo31@gmail.com>
2023-06-16 17:29:55 +0700

    update details UI, add footer

--- src/components/organisms/Footer/index.jsx
+++ src/components/organisms/Footer/index.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="text-light text-center mt-4">
+      <div className="container py-4">
+        <ul className="nav justify-content-center">
+          <li className="nav-item">
+            <p className="nav-link text-light">Dinda Friska Oktaviana</p>
+          </li>
+          <li className="nav-item">
+            <p className="nav-link text-light">Dirham Triyadi</p>
+          </li>
+          <li className="nav-item">
+            <p className="nav-link text-light">Muhammad Elham Al Haq</p>
+          </li>
+        </ul>
+      </div>
+      <div className="text-white pb-2">
+        <p className="mt-0">© 2023 Kelompok 4</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
